2111506f899663c868e5680f17b1e454e74252f9
aniketpatil14 <patilanian18it@student.mes.ac.in>
2020-10-01 14:49:52 +0530

    modified

--- Day_2/q6_1_special_variables_html.php
+++ Day_2/q6_1_special_variables_html.php
@@ -2,30 +2,39 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<form action="q6_1_special_variables_html.php" method="GET">
-			<label for="side1">side1 :</label><br>
-			<input type="text" id="side1" name="side1"><br>
-			<label for="side2">side2 :</label><br>
-			<input type="text" id="side2" name="side2"><br>
-			<label for="side3">side3 :</label><br>
-			<input type="text" id="side3 "name="side3"><br><br>
-			<input type="submit" value="Submit">
-		</form>
+		<div align="center">
+			<h2>Enter ther sides of Triangle</h2>
+			<form action="q6_1_special_variables_html.php" method="GET">
+				<label for="side1">side1 :</label><br>
+				<input type="text" id="side1" name="side1"><br>
+				<label for="side2">side2 :</label><br>
+				<input type="text" id="side2" name="side2"><br>
+				<label for="side3">side3 :</label><br>
+				<input type="text" id="side3 "name="side3"><br><br>
+				<input type="submit" value="Submit">
+			</form>
+		</div>
 	</body>
 </html>
-<?php 
+<div align="center">
+	<h3>
+		<?php 
 
-	if(!empty($_GET['side1'])){
-		$a = $_GET['side1'];
-		$b = $_GET['side2'];
-		$c = $_GET['side3'];
-		echo "Triangle is ";
-		if($a == $b && $b == $c)
-			echo "Equilateral";
-		elseif($a != $b && $b != $c && $a != $c)
-			echo "Scalene";
-		else
-			echo "Isosceles";
-	}
+			if(!empty($_GET['side1'])){
+				$a = $_GET['side1'];
+				$b = $_GET['side2'];
+				$c = $_GET['side3'];
+				echo "Triangle is ";
+				if($a == $b && $b == $c)
+					echo "Equilateral";
+				elseif ((($a**2 + $b**2) == $c**2) || (($b**2 + $c**2) == $a**2) || (($a**2 + $c**2) == $b**2))
+					echo "Right Angled And Scalene Triangle";
+				elseif($a != $b && $b != $c && $a != $c)
+					echo "Scalene";
+				else
+					echo "Isosceles";
+			}
 
-?>
+		?>
+	</h3>
+</div>
--- Day_2/q6_2_special_variables_html.php
+++ Day_2/q6_2_special_variables_html.php
@@ -1,38 +1,41 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<form action="q6_2_special_variables_html.php" method="GET">
-			<label for="name">Name of Student : </label>
-			<input type="text" id="name" name="name" required><br>
-			<p>Enter Marks</p>
-			<label for="Physics">Physics :</label><br>
-			<input type="text" id="Physics" name="Physics" required> /100<br>
-			<label for="Chemistry">Chemistry :</label><br>
-			<input type="text" id="Chemistry" name="Chemistry" required> /100<br>
-			<label for="Mathematics">Mathematics :</label><br>
-			<input type="text" id="Mathematics" name="Mathematics" required> /100<br>
-			<label for="Pilitical">Political Science :</label><br>
-			<input type="text" id="Political" name="Political" required> /100<br>
-			<label for="Geography">Geography :</label><br>
-			<input type="input" id="Geography" name="Geography" required> /100<br><br>
-			<input type="submit" name="Submit">
-		</form>
+		<div align="center">
+			<form action="q6_2_special_variables_html.php" method="POST">
+				<label for="name">Name of Student : </label>
+				<input type="text" id="name" name="name" required><br>
+				<p>Enter Marks</p>
+				<label for="Physics">Physics :</label><br>
+				<input type="number" id="Physics" name="Physics" min="0" max="100" required><br>
+				<label for="Chemistry">Chemistry :</label><br>
+				<input type="number" id="Chemistry" name="Chemistry" min="0" max="100" required><br>
+				<label for="Mathematics">Mathematics :</label><br>
+				<input type="number" id="Mathematics" name="Mathematics" min="0" max="100" required><br>
+				<label for="Political">Political Science :</label><br>
+				<input type="number" id="Political" name="Political" min="0" max="100" required><br>
+				<label for="Geography">Geography :</label><br>
+				<input type="number" id="Geography" name="Geography" min="0" max="100" required><br><br>
+				<input type="submit" name="Submit">
+			</form>
+		</div>
 	</body>
 </html>
+
 <?php 
 
-	if(!empty($_GET['name'])){
-		echo "<br>Name : ".$_GET['name']."<br>";
-		echo "<br>Marks in Each Subject<br>";
-		echo "<br>Physics           : ".$_GET['Physics'];
-		echo "<br>Chemistry         : ".$_GET['Chemistry'];
-		echo "<br>Mathematics       : ".$_GET['Mathematics'];
-		echo "<br>Pilitical Science : ".$_GET['Political'];
-		echo "<br>Geography 		: ".$_GET['Geography']."<br>";
-		$Total = $_GET['Physics']+$_GET['Chemistry']+$_GET['Mathematics']+$_GET['Political']+$_GET['Geography'];
+	if(!empty($_POST['name'])){
+		echo "<h2><br>Name : ".$_POST['name']."<br></h2>";
+		echo "<h3><br>Marks in Each Subject<br>";
+		echo "<br>Physics           : ".$_POST['Physics'];
+		echo "<br>Chemistry         : ".$_POST['Chemistry'];
+		echo "<br>Mathematics       : ".$_POST['Mathematics'];
+		echo "<br>Political Science : ".$_POST['Political'];
+		echo "<br>Geography 		: ".$_POST['Geography']."<br>";
+		$Total = $_POST['Physics']+$_POST['Chemistry']+$_POST['Mathematics']+$_POST['Political']+$_POST['Geography'];
 		echo "<br>Total Marks Obtained : ".$Total;
-		echo "<br>Total Marks : 500";
-		echo "<br>Percentage : ".$Total/500*100;
+		echo "<br>Total Marks : 500</h3>";
+		echo "<h3><br>Percentage : ".$Total/500*100;
 	}
 
 ?>
